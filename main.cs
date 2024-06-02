c9b130f76f3deca58d6c634e621b78d4050376b3
Eva Mae Miller <150403453+evmaemiller@users.noreply.github.com>
2024-06-02 21:13:46 +0000

    For Unit Test #3, changed the units of inches

--- main.cs
+++ main.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        double inches = 50;
+        double inches = -15;
         double centimeters = inches * 2.54;
 
         Console.WriteLine("{0} inches is equal to {1} centimeters.", inches, centimeters);
