ed5189448bc6ec2d876a8eac24bc324176354d9f
Eva Mae Miller <150403453+evmaemiller@users.noreply.github.com>
2024-06-02 21:09:39 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double inches = 5;
+        double centimeters = inches * 2.54;
+
+        Console.WriteLine("{0} inches is equal to {1} centimeters.", inches, centimeters);
+    }
+}
